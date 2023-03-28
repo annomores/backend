541a673cb668f51ef8f082bcfe01f637681eee29
annomores <112850461+annomores@users.noreply.github.com>
2023-03-28 15:57:49 +0300

    Update form.php

--- 3/form.php
+++ 3/form.php
@@ -7,7 +7,17 @@
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
         rel="stylesheet"
-          
+      <style>
+
+        html, body {
+            height: 100%;
+        }
+
+        body {
+            background-color: purple-100;
+        }
+
+    </style>     
 </head>
    
 <body>
