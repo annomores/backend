b37cf75964c7fa3192da702597185ed1992575b8
annomores <112850461+annomores@users.noreply.github.com>
2023-05-23 01:00:31 +0300

    Update login.php

--- 5/login.php
+++ 5/login.php
@@ -1,87 +1,63 @@
-<?php
-
-header('Content-Type: text/html; charset=UTF-8');
-
-session_start();
-
-if (!empty($_SESSION['login'])) {
-    print('
-    <form method="POST">
-        <div class="d-grid gap-2 col-6 mx-auto">
-            <input class="btn btn-light" type="submit" name="sessiondestroy" value="Выход"/>
-        </div>
+<html>
+<header>
+  <link rel="stylesheet" href="style.css" type="text/css">
+</header>
+  <?php
+  header('Content-Type: text/html; charset=UTF-8');
+  session_start();
+  if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (!empty($_SESSION['login'])) {
+    header('Location: index.php');
+    }else{
+  ?>
+  <body>
+    <form action="login.php" method="post">
+      <br>Логин
+      <input name="login" placeholder="login">
+      Пароль
+      <input name="password" type="password" placeholder="password">
+      <input type="submit" value="Войти" />
     </form>
-    ');
+  </body>
+</html>
+<?php
+  }
 }
-
-if( isset( $_POST['sessiondestroy'] ) ) {
-    session_destroy();
-    header('Location: ./');
-    exit();
-}
-
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Lab 5</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-              rel="stylesheet"
-              integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-              crossorigin="anonymous"
-        >
-    </head>
-    <body>
-    <div class="container">
-        <form action="" method="post">
-            <div class="mb-3">
-                <label for="login" class="form-label">Ваш логин</label>
-                <input type="login" class="form-control" name="login" id="login" aria-describedby="Enter your login">
-            </div>
-            <div class="mb-3">
-                <label for="pwd" class="form-label">Ваш пароль</label>
-                <input type="pwd" class="form-control" name="pwd" id="pwd">
-            </div>
-            <input type="submit" class="btn btn-primary" value="Войти"/>
-        </form>
-    </div>
-    <footer>
-        
-    </footer>
-    </body>
-    </html>
-    <?php
-}
+// Иначе, если запрос был методом POST, т.е. нужно сделать авторизацию с записью логина в сессию.
 else {
-    $user = 'u52822';
-    $pass = '8321484';
-    $db = new PDO('mysql:host=localhost;dbname=u52822', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
-    try {
-        $stmt = $db->prepare("SELECT * FROM login WHERE login=:login");
-        $stmt->execute(array("login"=>$_POST['login']));
-        $data = current($stmt->fetchAll(PDO::FETCH_ASSOC));
+  $login=$_POST['login'];
+  $password=$_POST['password'];
+  $uid=0;
+  $error=TRUE;
+  $user = 'u52805';
+  $pass = '5816061';
+  $db1 = new PDO('mysql:host=localhost;dbname=u52805', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  if(!empty($login) and !empty($password)){
+    try{
+      $chk=$db1->prepare("SELECT * FROM user_info WHERE login=?");
+      $chk->bindParam(1,$login);
+      $chk->execute();
+      $username=$chk->fetchALL();
+	  print($username[0]['password']);
+      if(password_verify($password,$username[0]['password'])){
+        $uid=$username[0]['id'];
+        $error=FALSE;
+      }
     }
-    catch(PDOException $e) {
-        print('Error : ' . $e->getMessage());
-        exit();
+    catch(PDOException $e){
+      print('Error : ' . $e->getMessage());
+      exit();
     }
-
-    if (empty($data)){
-        print('<div class="alert alert-secondary" role="alert">Пользователь с таким именем или паролем не найден</div>');
-    }
-    else {
-        if (password_verify($_POST['pwd'], $data['pwd'])) {
-            $_SESSION['uid'] = $data['user_id'];
-            $_SESSION['login'] = $_POST['login'];
-            header('Location: ./');
-            exit();
-        } else {
-            print('<div class="alert alert-secondary" role="alert">Неверный пароль</div>');
-        }
-    }
-
+  }
+  if($error==TRUE){
+    print('<br>Неверный логин/пароль. <br> Создайте нового <a href="index.php" style="color:black;">пользователя</a> или попытайтесь <a href="login.php"style="color:black;">войти</a> снова');
+    session_destroy();
     exit();
+  }
+  // Если все ок, то авторизуем пользователя.
+  $_SESSION['login'] = $login;
+  // Записываем ID пользователя.
+  $_SESSION['uid'] = $uid;
+  // Делаем перенаправление.
+  header('Location: index.php');
 }
-?>
