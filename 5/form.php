1936200b6b39e505ada7a2dd6b3c0d9b0d860712
annomores <112850461+annomores@users.noreply.github.com>
2023-05-23 00:07:26 +0300

    Update form.php

--- 5/form.php
+++ 5/form.php
@@ -2,7 +2,7 @@
 <html lang="ru">
 <head>
   <meta charset="utf-8">
-  <title>Lab 4</title>
+  <title>Lab 5</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
         rel="stylesheet">
   <style>
